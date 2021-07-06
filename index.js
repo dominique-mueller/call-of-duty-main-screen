05898c2e1d4d485f7d95e0df08bda9d9db076b1a
Dominique Müller <dominique.m.mueller@gmail.com>
2021-07-06 20:01:14 +0200

    Refactor to simplified variant

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+// Get game elements
 const gameElements = document.querySelectorAll('[data-id="game"]');
 
-console.log(gameElements);
-
+// Activate game element on hover
 Array.from(gameElements).forEach((gameElement) => {
-  gameElement.addEventListener('mouseover', (event) => {
+  gameElement.addEventListener('mouseover', () => {
+    // Add 'is-active' class to this element
+
     gameElement.classList.add('is-active');
+    // Remove 'is-active' classes from other elements
     Array.from(gameElements)
       .filter((globalGameElement) => {
         return globalGameElement !== gameElement;
