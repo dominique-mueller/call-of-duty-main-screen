ed53ade4975801297b6a2f1fffb3a8f6205ef99c
Dominique Müller <dominique.m.mueller@gmail.com>
2021-07-06 19:01:58 +0200

    First experiments

--- README.md
+++ README.md
@@ -5,3 +5,12 @@
 **A recreation of the Call of Duty main screen in HTML / CSS / JavaScript.**
 
 </div>
+
+## Asset sources
+
+- Call of Duty: Modern Warfare (2019)<br>
+  https://wall.alphacoders.com/big.php?i=1021954
+- Call of Duty: Black Ops - Cold War<br>
+  https://wall.alphacoders.com/big.php?i=1098057
+- Call of Duty: Warzone<br>
+  https://wall.alphacoders.com/big.php?i=1081975
--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('TEST')
+const gameElements = document.querySelectorAll('[data-id="game"]');
+
+console.log(gameElements);
+
+Array.from(gameElements).forEach((gameElement) => {
+  gameElement.addEventListener('mouseover', (event) => {
+    event.target.classList.add('is-active');
+    Array.from(gameElements)
+      .filter((gameElement) => {
+        return gameElement !== event.target;
+      })
+      .forEach((otherGameElemenet) => {
+        otherGameElemenet.classList.remove('is-active');
+      });
+  });
+});
